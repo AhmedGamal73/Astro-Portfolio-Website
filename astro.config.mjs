ba83a1b9ca4d1ce01369625253062a04db7ddb75
Ake Jamal <malokokha2@gmail.com>
2023-07-03 14:05:42 +0300

    setup netfly cofigirations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://AhmedGamal73.github.io',
-  base: '/Astro-Portfolio-Website',
   integrations: [react()],
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
