22617721adcdbae8604e26cc744dded3a878f3e0
Ake Jamal <malokokha2@gmail.com>
2023-07-03 13:51:15 +0300

    add project to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AhmedGamal73.github.io',
+  base: '/Astro-Portfolio-Website',
   integrations: [react()],
   output: 'server',
   adapter: node({
