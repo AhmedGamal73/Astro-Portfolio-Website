6c35e31123f1a239b8d4c1027deecf5f85f6ed63
AhmedGamal73 <malokokha2@gmail.com>
2023-07-12 18:24:20 +0300

    deployment issue2

--- .vercel/output/static/_astro/FAskedQuestions.229adbcc.js
+++
@@ -1,1 +0,0 @@
-import{default as i}from"./FAskedQuestions.bda88319.js";import"./index.a32aa79c.js";import"./index.52baf61d.js";import"./jsx-runtime.210dbaa9.js";export{i as default};
--- .vercel/output/static/_astro/hoisted.4d701f62.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",()=>{const d=document.querySelector(".loader");d.classList.add("hidden"),d.addEventListener("transitionend",()=>{document.body.removeChild(d)})});
