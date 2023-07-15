3e7fb6de4021aab96047141edf9837757f66c021
AhmedGamal73 <malokokha2@gmail.com>
2023-07-15 18:07:40 +0300

    add content to service page

---
+++ src/components/FAQs/AuditFAQ .jsx
@@ -0,0 +1,74 @@
+import Faq from "react-faq-component";
+
+const data = {
+  title: "",
+  rows: [
+    {
+      title: "What is Bookkeeping ?",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
+                ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
+                In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
+                Fusce sed commodo purus, at tempus turpis.`,
+    },
+    {
+      title: "Why I need to focus in this area ?",
+      content:
+        "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+    },
+    {
+      title: "What does a bookkeeper do?",
+      content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+    },
+    {
+      title: "What is the package version",
+      content: <p>current version is 1.2.1</p>,
+    },
+    {
+      title: "Should I do my own bookkeeping?",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
+                ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
+                In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
+                Fusce sed commodo purus, at tempus turpis.`,
+    },
+    {
+      title: "What is key benefits of bookkeeping ?",
+      content:
+        "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+    },
+    {
+      title: "Curabitur laoreet, mauris vel blandit fringilla",
+      content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+    },
+    {
+      title: "What is the package version",
+      content: <p>current version is 1.2.1</p>,
+    },
+  ],
+};
+
+const styles = {
+  bgColor: "transparent",
+  titleTextColor: "var(--prim1)",
+  rowTitleColor: "var(--prim2)",
+  rowContentColor: "grey",
+  arrowColor: "var(--prim2)",
+};
+
+const config = {
+  animate: true,
+  expandIcon: "+",
+  collapseIcon: "-",
+  tabFocus: true,
+};
+
+const AuditFAQ = () => {
+  return <Faq data={data} styles={styles} config={config} />;
+};
+
+export default AuditFAQ;
