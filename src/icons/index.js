732b0316fe150a8fc01d3206e588a4a5a10cae18
AhmedGamal73 <malokokha2@gmail.com>
2023-08-03 16:47:58 +0300

    change footer

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,19 +1,7 @@
 import facebook from "./facebook.svg";
 import linkedin from "./linkedin.svg";
 import instagram from "./instagram.svg";
+import footerLogo from "./footer-logo.webp";
 import logo from "./logo.webp";
-import client1 from "./clint1.svg";
-import client2 from "./clint2.svg";
-import client3 from "./clint3.svg";
-import client4 from "./clint4.svg";
 
-export {
-  logo,
-  facebook,
-  linkedin,
-  instagram,
-  client1,
-  client2,
-  client3,
-  client4,
-};
+export { footerLogo, logo, facebook, linkedin, instagram };
